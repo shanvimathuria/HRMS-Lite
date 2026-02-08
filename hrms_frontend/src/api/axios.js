1e5e6468fbdb7e100cd45a8b505ad020f8801297
Shanvi Mathuria <124160341+shanvimathuria@users.noreply.github.com>
2026-02-09 00:39:16 +0530

    env updated

--- hrms_frontend/src/api/axios.js
+++ hrms_frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hrms-lite-backend-o8u0.onrender.com",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
